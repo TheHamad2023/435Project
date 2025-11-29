a57a431016c46cd96a6652547ea13ffbd19a02b5
Fahris Nassief <fahris.nassief@colostate.edu>
2025-11-29 14:48:11 -0700

    Setup project with Maven and Spark. Built initial Spark Application and set schema + read in raw data.

---
+++ src/main/java/TwitchDataPipeline.java
@@ -0,0 +1,44 @@
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.types.DataTypes;
+import org.apache.spark.sql.types.StructType;
+
+public class TwitchDataPipeline {
+
+    // Dataset coverage start (UTC): 2021-07-31 00:00:00
+    // Epoch seconds for 2021-07-31 00:00:00 = 1627689600
+    // Used for viewer timestamps
+    private static final long START_EPOCH_UTC = 1627689600L;
+
+    public static void main(String[] args) {
+
+        // cmd-line args
+        if (args.length != 1) {
+            System.err.println("Usage: TwitchDataPipeline-1.0 <input_file>");
+            System.exit(1);
+        }
+
+        String inputFile = args[0];
+
+        // Spark session
+        SparkSession spark = SparkSession.builder()
+                .appName("TwitchDataPipeline")
+                .config("spark.sql.session.timeZone", "UTC")
+                .getOrCreate();
+
+        // Define data schema since Kaggle dataset doesn't include headers
+        StructType schema = new StructType()
+                .add("user_id", DataTypes.StringType)
+                .add("stream_id", DataTypes.StringType)
+                .add("streamer_username", DataTypes.StringType)
+                .add("time_start", DataTypes.LongType)
+                .add("time_stop", DataTypes.LongType);
+
+        // Load initial raw data
+        Dataset<Row> rawData = spark.read()
+                .option("header", "false")
+                .schema(schema)
+                .csv(inputFile);
+    }
+}
